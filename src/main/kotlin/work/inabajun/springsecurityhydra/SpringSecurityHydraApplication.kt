b891af2811627089dac9b9325511a400ca4c50b3
inabajunmr <inabajun.for.regi@gmail.com>
2021-08-02 18:35:08 +0900

    init

---
+++ src/main/kotlin/work/inabajun/springsecurityhydra/SpringSecurityHydraApplication.kt
@@ -0,0 +1,11 @@
+package work.inabajun.springsecurityhydra
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityHydraApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringSecurityHydraApplication>(*args)
+}
---
+++ src/test/kotlin/work/inabajun/springsecurityhydra/SpringSecurityHydraApplicationTests.kt
@@ -0,0 +1,13 @@
+package work.inabajun.springsecurityhydra
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSecurityHydraApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
